cd7ab09fe5dfb15668be8f14fb8447bc1c7a6985
dhelo11 <dhelo11@No-Mercy>
2024-03-22 08:41:04 +0000

    drivers/zram: Force default zram to 4GB

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1816,9 +1816,8 @@ static ssize_t disksize_store(struct device *dev,
 	struct zram *zram = dev_to_zram(dev);
 	int err;
 
-	disksize = memparse(buf, NULL);
-	if (!disksize)
-		return -EINVAL;
+	disksize = (u64)SZ_1G * 4;
+	pr_info("Overriding zram size to %li", disksize);
 
 	down_write(&zram->init_lock);
 	if (init_done(zram)) {
